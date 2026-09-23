cd93d1f856d14a3ae8edcd75a4e83a2c77c9500f
CleverForge <90665723+cleverforge@users.noreply.github.com>
2026-09-23 12:28:00 -0230

    security: register granular CleverForms admin permissions

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,24 @@ import controllers from './controllers';
 import routes from './routes';
 import services from './services';
 
+const permissions = [
+  'forms.read',
+  'forms.create',
+  'forms.update',
+  'forms.delete',
+  'forms.publish',
+  'submissions.read',
+].map((action) => ({
+  section: 'plugins',
+  displayName: action.replace('.', ' '),
+  uid: action,
+  pluginName: 'clever-forms',
+}));
+
 export default {
-  register() {},
+  async register({ strapi }: { strapi: any }) {
+    await strapi.admin.services.permission.actionProvider.registerMany(permissions);
+  },
   bootstrap() {},
   config: { default: {}, validator() {} },
   contentTypes,
